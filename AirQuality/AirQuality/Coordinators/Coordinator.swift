7d7902c80a9f08de00b6c8d83956329ed91170ee
Bartosz Dolewski <bartosz.dolewski@gmail.com>
2019-01-13 20:20:09 +0100

    Clean up in Coordinators

--- AirQuality/AirQuality/Coordinators/Coordinator.swift
+++ AirQuality/AirQuality/Coordinators/Coordinator.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-protocol Coordinator: class {
+protocol Coordinator: AnyObject {
     var rootViewController: UIViewController { get }
     func start()
 }
--- AirQuality/AirQuality/Coordinators/SearchCoordinator.swift
+++ AirQuality/AirQuality/Coordinators/SearchCoordinator.swift
@@ -16,58 +16,24 @@ struct Station {
     let address: String?
 }
 
-protocol SearchCoordinatorDelegate: AnyObject {
-    func passStation(station: Station)
-}
-
 class SearchCoordinator: Coordinator {
     var rootViewController: UIViewController {
         return navigationController
     }
     
-    var delegate: SearchCoordinatorDelegate? {
-        didSet {
-            print("SearchCoordinatorDelegate is set")
-        }
-    }
-    
     private var navigationController: UINavigationController
-    private let viewController: SearchViewController
-    private let disposeBag = DisposeBag()
     
     init() {
         self.navigationController = UINavigationController()
-        self.viewController = SearchViewController.instantiate(SearchViewController.self)
     }
     
     public func start() {
-        //let searchVC = SearchViewController.instantiate(SearchViewController.self)
+        let searchVC = SearchViewController.instantiate(SearchViewController.self)
         let searchVM = SearchViewModel()
         
-        self.viewController.viewModel = searchVM
-        self.viewController.delegate = self
-        self.viewController.title = "Search station"
-        self.viewController.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 1)
+        searchVC.viewModel = searchVM
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 1)
         
-        self.navigationController.pushViewController(self.viewController, animated: false)
-    }
-}
-
-extension SearchCoordinator: SearchViewControllerDelegate {
-    func didSelected(station: Station) {
-        if self.delegate == nil {
-            print("delegate == nil")
-            return
-        }
-        
-        delegate?.passStation(station: station)
-        //self.delegate?.didSelected(station: station)
-    }
-}
-
-extension SearchCoordinator {
-    static func debug(station: Station) {
-        print(#function, String(describing: self))
-        dump(station)
+        self.navigationController.pushViewController(searchVC, animated: false)
     }
 }
